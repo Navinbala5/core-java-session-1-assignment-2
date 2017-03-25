b7fba8bd9e5eec9df96ecb71a2e6741ad55fb7e0
Navinbala5 <bnavin28@gmail.com>
2017-03-25 08:55:05 +0530

    Add files via upload

---
+++ SecondAssignment.java
@@ -0,0 +1,26 @@
+
+public class SecondAssignment {
+	
+
+	public static void main(String[] args) {
+	
+		int a,b,c,d,e;
+		a=20;
+		b=10;
+		
+		b=a-- - --a;
+		c= a--;
+		d=a>>2;
+		e=a&b;
+		
+		System.out.println("the sum of two integer is " +b);
+		System.out.println("the sum of two integer is " +c);
+		System.out.println("the sum of two integer is " +d);
+		System.out.println("the sum of two integer is " +e);
+		
+		
+	
+		
+	}
+
+}
